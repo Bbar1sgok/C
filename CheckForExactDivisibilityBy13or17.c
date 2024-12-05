cca6aa797b2b2698c4aeca4daf36b27dcf9cde58
Bayram Barış Gök <138613519+Bbar1sgok@users.noreply.github.com>
2024-12-05 11:43:43 +0300

    Update CheckForExactDivisibilityBy13or17.c

--- CheckForExactDivisibilityBy13or17.c
+++ CheckForExactDivisibilityBy13or17.c
@@ -13,17 +13,14 @@ int main() {
     if(girilenSayi % 13 == 0 && girilenSayi % 17 == 0){
 
         printf("Girilen sayi: %d, 13 ve 17 sayilarina tam bolunur.", girilenSayi);
-
     }
     else if(girilenSayi % 13 == 0){
-
+        
         printf("Girilen sayi: %d, sadece 13 e tam bolunur. ", girilenSayi);
-
     }
     else if(girilenSayi % 17 == 0){
 
         printf("Girilen sayi: %d, sadece 17 e tam bolunur. ", girilenSayi);
-
     }
     else{
 

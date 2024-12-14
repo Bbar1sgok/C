02a62d3687850e21a33a5ab615b9ee86b83b13ae
Bayram Barış Gök <138613519+Bbar1sgok@users.noreply.github.com>
2024-12-14 15:42:37 +0300

    Create 2_3_6_divisibility.c

---
+++ 2_3_6_divisibility.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// İkinin katı mı diye kontrol ediyoruz
+
+int ikininKatiMi(int sayi){
+
+    if(sayi % 2 == 0){
+
+        return 1;
+    }
+    else{
+
+        return 0;
+    }
+
+
+}
+
+// Üçün katı mı diye kontrol ediyoruz
+
+int ucunKatiMi(int sayi){
+
+    if(sayi % 3 == 0){
+
+        return 1;
+    }
+    else{
+
+        return 0;
+    }
+
+}
+
+
+
+int main()
+{
+    int girilenSayi;
+
+    printf("Lutfen bir tam sayi giriniz:\n");
+    scanf("%d", &girilenSayi);
+
+    if(ikininKatiMi(girilenSayi) == 1){
+
+        printf("Sayimiz cift sayidir.\n");
+    }
+    
+    if(ucunKatiMi(girilenSayi) == 1){
+
+        printf("Sayimiz 3 ile tam  bolunur.\n");
+    }
+    
+    if(ikininKatiMi(girilenSayi) == 1 && ucunKatiMi(girilenSayi) == 1){
+
+        printf("Sayimiz 6 ile tam bolunur.");
+    }
+    return 0;
+}
